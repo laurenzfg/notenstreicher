a03edeee2029b9c0194ce568bff3c29f49f7302d
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-12-22 17:28:59 +0100

    Upload Source Code

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# notenstreicher
+# Notenstreicher
+
+Notenstreicher is a utility to decide which grades to cancel from your final GPA in the context of the *Streichregel*
+of the Computer Science B.Sc. programme of RWTH Aachen university.
+
+In this degree programme, you may cancel at most one course in every focus area from the final GPA.
+You may cancel courses to a maximum of 30 ECTS.
+
+Just give this program your full transcript and it will tell you the optimal cancellation decision.
+Although the algorithm should be correct, *I do not accept responsibility if the program returns you a non-optimal recommendation*.
+
+## How to invoke the program?
+
+Download a Go runtime from the [Go Project](https://go.dev) and invoke my program with
+
+    go run notenstreicher.go
+
+Insert your transcript except for the Bachelor Thesis (it can not be canceled) in the following format:
+* Every block starts with the name of focus area (e.g Practical CS)
+* All grades in a block are given in the format 'Name:ECTS:Grade' e.g. (Programming:8:1.3)
+* Blocks are terminated with a single blank line
+* Spaces are ignored; The input must not contain tabs or colons except where specified
+* Input is terminated with a double blank line
+
+An example input:
+
+    Praktische Informatik
+    Programmierung:8:2.0
+    DSAL:8:2.0
+    Datenbanken:6:2.0
+    SWT:6:2.0
+
+    Technische Informatik
+    TI:6:2.0
+    BUS:6:2.0
+    Datkom:6:2.0
+
+    Theoretische Informatik
+    Fosap:6:2.0
+    BuK:7:2.0
+    Malo:7:4.0
+
+    Mathematik
+    DS:6:2.0
+    AfI:8:2.0
+    LA:6:2.0
+    Stocha:6:2.0
+
+    Sonstiges
+    Proseminar:3:2.0
+    Praktikum:6:2.0
+    Seminar:5:2.0
+
+    Wahlpflicht
+    DPN:6:2.0
+    AI:6:2.0
+    Compilerbau:6:2.0
+
+    Anwendungsfach
+    NumA1:6:2.0
+    Mathe Praktikum:6:2.0
+    Cobra:10:2.0
+
+## How can I modify the program?
+I hereby put the software project "notenstreicher" authored by laurenzfg in the last days of 2021 under the MIT License.
+See LICENSE file for details.
+
+So feel free to fix and improve the program!
+Small changes in the degree programme structure should only require different input,
+while fundamental changes to the cancellation logic might require source code changes.
+
+If you have a productive bugfix or feature improvement, create a PR or reach out to me via my [homepage](https://laurenzfg.com)
---
+++ notenstreicher.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var maxECTSCancel = 30
+
+// Every Grade and every MutuallyExclusiveGradesGroup is uniquely identified by its' name
+
+type Transcript struct {
+	MEGGs []*MutuallyExclusiveGradesGroup `json:"meggs"`
+}
+
+type MutuallyExclusiveGradesGroup struct {
+	Name   string   `json:"name"`
+	Grades []*Grade `json:"grades"`
+}
+
+type Grade struct {
+	Name  string  `json:"name"`
+	Grade float64 `json:"grade"`
+	ECTS  int     `json:"ects"`
+}
+
+func (tr *Transcript) avg() float64 {
+	meggCount := len(tr.MEGGs)
+
+	// Ignore no grades:
+	noopIgnoreList := make([]int, meggCount)
+	for i, _ := range noopIgnoreList {
+		noopIgnoreList[i] = -1
+	}
+
+	return tr.avgWithIgnoreList(noopIgnoreList)
+}
+
+// This calculates the average, but ignoring at most one grade per MEGG
+// So iff ignoreList[i] = j, the jth grade in the 1st MEGG is ignored in
+// the average calculation.
+// If you want to include a full MEGG in the average calculation, set ignoreList[i] = -1
+func (tr *Transcript) avgWithIgnoreList(ignoreList []int) float64 {
+	// Assert ignoreList is in the correct shape
+	if len(ignoreList) != len(tr.MEGGs) {
+		fmt.Println("WARN: ignore list is of wrong shape")
+		return 6.0
+	}
+
+	sum := 0.0
+	ectsSum := 0.0
+
+	for i, megg := range tr.MEGGs {
+		for j, grade := range megg.Grades {
+			if ignoreList[i] == j {
+				// Skip cancelled grades in avg calculation
+				continue
+			}
+			ectsAsFloat := float64(grade.ECTS) // to avoid unwanted int converts
+			sum += grade.Grade * ectsAsFloat
+			ectsSum += ectsAsFloat
+		}
+	}
+
+	if ectsSum == 0.0 {
+		// Blank transcript ==> return 6.0 as fallback value
+		return 6.0
+	}
+	return sum / ectsSum
+}
+
+func (tr *Transcript) findOptimalIgnoreList() (optimalIgnoreList []int, optimalAVG float64) {
+	// Start the recursion with the appropriate ectsLimit, an all -1 ignoreListSoFar and curPos=0
+	initialIgnoreList := make([]int, len(tr.MEGGs))
+	for i, _ := range initialIgnoreList {
+		initialIgnoreList[i] = -1
+	}
+
+	return tr.findOptimalIgnoreListRecursion(initialIgnoreList, maxECTSCancel, 0)
+}
+
+// Start the recursion with the appropriate ectsLimit, an all -1 ignoreListSoFar and curPos=0
+// Solves the optimisation problem by doing a full Depth-First-Search
+func (tr *Transcript) findOptimalIgnoreListRecursion(ignoreListSoFar []int, ectsLimit int, curPos int) (optimalIgnoreList []int, optimalAVG float64) {
+	// We cannot recurse any further if we have no ECTS to cancel left or arrived at the last MEGG
+	if ectsLimit < 1 || curPos == len(ignoreListSoFar) {
+		return ignoreListSoFar, tr.avgWithIgnoreList(ignoreListSoFar)
+	}
+
+	// We go down the tree and evaluate to
+	// a) not cancel anything in the MEGG curPos
+	// b) cancel a grade in the MEGG curPos
+	// Set a) is the initial value for the returned maximum
+	optimalIgnoreList = make([]int, len(tr.MEGGs))
+	copy(optimalIgnoreList, ignoreListSoFar)
+	optimalIgnoreList, optimalAVG = tr.findOptimalIgnoreListRecursion(optimalIgnoreList, ectsLimit, curPos+1)
+
+	for i := 0; i < len(tr.MEGGs[curPos].Grades); i++ {
+		evaluatedIgnoreList := make([]int, len(tr.MEGGs))
+		copy(evaluatedIgnoreList, ignoreListSoFar)
+		// Set grade i as ignored
+		evaluatedIgnoreList[curPos] = i
+		nextEctsLimit := ectsLimit - tr.MEGGs[curPos].Grades[i].ECTS
+		// if we have cancelled more than ECTS available, skip this cancellation
+		if nextEctsLimit < 0 {
+			continue
+		}
+
+		evaluatedIgnoreList, evaluatedAVG := tr.findOptimalIgnoreListRecursion(evaluatedIgnoreList, nextEctsLimit, curPos+1)
+
+		if evaluatedAVG < optimalAVG {
+			optimalAVG = evaluatedAVG
+			optimalIgnoreList = evaluatedIgnoreList
+		}
+	}
+
+	return
+}
+
+
+func main() {
+	fmt.Println("Notenstreicher reads in a transcript as follows: MutuallyExclusiveGradesGroup name followed by Tricolons Name:ECTS:Grade\nA MutuallyExclusiveGradesGroup is closed with a blank line TODO output this helpstring\nA double blank line finishes input and calculates the grade average")
+	transcript := Transcript{}
+
+	subsequentBlankLines := 1
+	for subsequentBlankLines < 2 {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		text = strings.ReplaceAll(text, " ", "")
+		if text == "\n" {
+			subsequentBlankLines++
+			continue
+		}
+
+		if subsequentBlankLines == 1 {
+			// text has to be title of MutuallyExclusiveGradesGroup
+			transcript.MEGGs = append(transcript.MEGGs, &MutuallyExclusiveGradesGroup{Name: text[0 : len(text)-1]})
+		} else {
+			// text has to be title of MutuallyExclusiveGradesGroup Name:ECTS:Grade
+			textFields := strings.Split(text, ":")
+			if len(textFields) != 3 {
+				fmt.Printf("skipping processing of line %v, since it is not of form Name:ECTS:Grade\n", text[0:len(text)-1])
+				continue
+			}
+
+			name := textFields[0]
+			ects, err := strconv.Atoi(textFields[1])
+			if err != nil {
+				fmt.Printf("skipping processing of line %v, since ECTS is not an int\n", text[0:len(text)-1])
+				continue
+			}
+
+			gradeString := textFields[2] // has '\n' as last char
+			grade, err := strconv.ParseFloat(gradeString[0:len(gradeString)-1], 64)
+			if err != nil {
+				fmt.Printf("skipping processing of line %v, since grade is not a float\n", text[0:len(text)-1])
+				continue
+			}
+
+			// Add to transcript
+			mostCurrentMEGG := len(transcript.MEGGs) - 1
+			transcript.MEGGs[mostCurrentMEGG].Grades = append(transcript.MEGGs[mostCurrentMEGG].Grades,
+				&Grade{
+					Name: name, Grade: grade, ECTS: ects,
+				})
+		}
+		subsequentBlankLines = 0
+	}
+
+	fmt.Printf("your naïve average grade would be: %f\n", transcript.avg())
+
+	fmt.Println("Solving the optimisation problem")
+	optimalIgnoreList, optimalAvg := transcript.findOptimalIgnoreList()
+	fmt.Println("It is optimal to cancel the following grades:")
+	for meggI, j := range optimalIgnoreList {
+		meggName := transcript.MEGGs[meggI].Name
+		if j == -1 {
+			fmt.Printf("Don't cancel any grade in MEGG %s\n", meggName)
+		} else {
+			gradeName := transcript.MEGGs[meggI].Grades[j].Name
+			fmt.Printf("In MEGG %s, cancel %s\n", meggName, gradeName)
+		}
+	}
+	fmt.Printf("\nThis cancellation yields an average of: %f\n", optimalAvg)
+
+	fmt.Printf("\nlaurenzfg congratelates you to your successful CS degree.\nlaurenzfg does not take any responsibility for the grade cancellation recommodation.\nSolve the problem yourself, you literally have a CS degree now!\nArrivederci!\n")
+}
