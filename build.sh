77f81e46465319d959d69cf189950863af5908aa
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-12-26 15:59:12 +0100

    build.sh:: compile GoKernel

--- build.sh
+++ build.sh
@@ -2,9 +2,17 @@
 
 # The GitHub Action in .github/workflows/main.yml does this in the cloud + deploys to gh_pages branch --> sent to GH pages
 
+# If you run this scirpt after changes to the GoKernel, you may simply use 'npm start' / 'npm build'
+
 # abort on errors
 set -e
 # install dependencies
 npm install
 # build static files
 npm run build
+
+# Compile Go + copy appropriate WASM loader over
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" public/
+cd GoKernel
+GOOS=js GOARCH=wasm go build -o ../public/main.wasm
+cd -
