7e714fabf6910d2211ca7f4a1d0a7cdeb6751042
Laurenz <git@laurenzfg.com>
2023-09-06 17:24:45 +0200

    Neue bachelor po (#26)
    * PO Selector Buttons
    * Implement new PO

--- src/App.js
+++ src/App.js
@@ -8,18 +8,42 @@ import MEGGDisplay from "./MEGG";
 
 import "./App.css";
 
-const initialTranscript = JSON.parse(
-  '{"meggs":[{"name":"PraktischeInformatik","grades":[{"name":"Programmierung","grade":"2","ects":"8"},{"name":"DSAL","grade":"2","ects":"8"},{"name":"Datenbanken","grade":"2","ects":"6"},{"name":"SWT","grade":"2","ects":"6"}]},{"name":"TechnischeInformatik","grades":[{"name":"TI","grade":"2","ects":"6"},{"name":"BUS","grade":"2","ects":"6"},{"name":"Datkom","grade":"2","ects":"6"}]},{"name":"TheoretischeInformatik","grades":[{"name":"Fosap","grade":"2","ects":"6"},{"name":"BuK","grade":"2","ects":"7"},{"name":"Malo","grade":"4","ects":"7"}]},{"name":"Mathematik","grades":[{"name":"DS","grade":"2","ects":"6"},{"name":"AfI","grade":"2","ects":"8"},{"name":"LA","grade":"2","ects":"6"},{"name":"Stocha","grade":"2","ects":"6"}]},{"name":"Sonstiges","grades":[{"name":"Proseminar","grade":"2","ects":"3"},{"name":"Seminar","grade":"2","ects":"5"}]},{"name":"Wahlpflicht","grades":[{"name":"DPN","grade":"2","ects":"6"},{"name":"AI","grade":"2","ects":"6"},{"name":"Compilerbau","grade":"2","ects":"6"},{"name":"CSE","grade":"2","ects":"6"}]},{"name":"Anwendungsfach","grades":[{"name":"NumA1","grade":"2","ects":"6"},{"name":"MathePraktikum","grade":"2","ects":"6"},{"name":"Cobra","grade":"2","ects":"10"}]}]}'
-);
+
+// POs are indixed as follow:
+// 0: BSc 2022
+// 1: BSc 2018
+// 2: MSc 2023
+// 3: MSc 2010
 
 const App = () => {
-  const [transcript, updateTranscript] = useState(initialTranscript);
+  const initialTranscript = {
+    "BSc 2022": JSON.parse(
+      '{ "meggs": [{ "name": "PraktischeInformatik", "grades": [{ "name": "Programmierung", "grade": "2", "ects": "8" }, { "name": "DSAL", "grade": "2", "ects": "7" }, { "name": "Datenbanken", "grade": "2", "ects": "6" }, { "name": "SWT", "grade": "2", "ects": "6" }, { "name": "Elements of Machine Learning and Data Science", "grade": "2", "ects": "6" }] }, { "name": "TechnischeInformatik", "grades": [{ "name": "TI", "grade": "2", "ects": "6" }, { "name": "BUS", "grade": "2", "ects": "7" }, { "name": "Datkom", "grade": "2", "ects": "6" }, { "name": "IT-Security", "grade": "2", "ects": "4" } ] }, { "name": "TheoretischeInformatik", "grades": [{ "name": "Fosap", "grade": "2", "ects": "6" }, { "name": "BuK", "grade": "2", "ects": "7" }, { "name": "Malo", "grade": "4", "ects": "7" }] }, { "name": "Mathematik", "grades": [{ "name": "DS", "grade": "2", "ects": "6" }, { "name": "AfI", "grade": "2", "ects": "8" }, { "name": "LA", "grade": "2", "ects": "6" }, { "name": "Stocha", "grade": "2", "ects": "6" }] }, { "name": "Sonstiges", "grades": [{ "name": "Proseminar", "grade": "2", "ects": "4" }, { "name": "Seminar", "grade": "2", "ects": "4" }] }, { "name": "Wahlpflicht / Anwendung", "grades": [{ "name": "DPN", "grade": "2", "ects": "6" }, { "name": "AI", "grade": "2", "ects": "6" }, { "name": "Compilerbau", "grade": "2", "ects": "6" }, { "name": "CSE", "grade": "2", "ects": "6" }, { "name": "NumA I", "grade": "2", "ects": "6" }, { "name": "Computeralgebra", "grade": "2", "ects": "6" }] } ] }'
+    ),
+    "BSc 2018": JSON.parse(
+      '{"meggs":[{"name":"PraktischeInformatik","grades":[{"name":"Programmierung","grade":"2","ects":"8"},{"name":"DSAL","grade":"2","ects":"8"},{"name":"Datenbanken","grade":"2","ects":"6"},{"name":"SWT","grade":"2","ects":"6"}]},{"name":"TechnischeInformatik","grades":[{"name":"TI","grade":"2","ects":"6"},{"name":"BUS","grade":"2","ects":"6"},{"name":"Datkom","grade":"2","ects":"6"}]},{"name":"TheoretischeInformatik","grades":[{"name":"Fosap","grade":"2","ects":"6"},{"name":"BuK","grade":"2","ects":"7"},{"name":"Malo","grade":"4","ects":"7"}]},{"name":"Mathematik","grades":[{"name":"DS","grade":"2","ects":"6"},{"name":"AfI","grade":"2","ects":"8"},{"name":"LA","grade":"2","ects":"6"},{"name":"Stocha","grade":"2","ects":"6"}]},{"name":"Sonstiges","grades":[{"name":"Proseminar","grade":"2","ects":"3"},{"name":"Seminar","grade":"2","ects":"5"}]},{"name":"Wahlpflicht","grades":[{"name":"DPN","grade":"2","ects":"6"},{"name":"AI","grade":"2","ects":"6"},{"name":"Compilerbau","grade":"2","ects":"6"},{"name":"CSE","grade":"2","ects":"6"}]},{"name":"Anwendungsfach","grades":[{"name":"NumA1","grade":"2","ects":"6"},{"name":"MathePraktikum","grade":"2","ects":"6"},{"name":"Cobra","grade":"2","ects":"10"}]}]}'
+    ),
+    // "MSc 2023": JSON.parse(
+    //   '{"meggs":[{"name":"PraktischeInformatik","grades":[{"name":"Programmierung","grade":"3","ects":"8"},{"name":"DSAL","grade":"2","ects":"8"},{"name":"Datenbanken","grade":"2","ects":"6"},{"name":"SWT","grade":"2","ects":"6"}]},{"name":"TechnischeInformatik","grades":[{"name":"TI","grade":"2","ects":"6"},{"name":"BUS","grade":"2","ects":"6"},{"name":"Datkom","grade":"2","ects":"6"}]},{"name":"TheoretischeInformatik","grades":[{"name":"Fosap","grade":"2","ects":"6"},{"name":"BuK","grade":"2","ects":"7"},{"name":"Malo","grade":"4","ects":"7"}]},{"name":"Mathematik","grades":[{"name":"DS","grade":"2","ects":"6"},{"name":"AfI","grade":"2","ects":"8"},{"name":"LA","grade":"2","ects":"6"},{"name":"Stocha","grade":"2","ects":"6"}]},{"name":"Sonstiges","grades":[{"name":"Proseminar","grade":"2","ects":"3"},{"name":"Seminar","grade":"2","ects":"5"}]},{"name":"Wahlpflicht","grades":[{"name":"DPN","grade":"2","ects":"6"},{"name":"AI","grade":"2","ects":"6"},{"name":"Compilerbau","grade":"2","ects":"6"},{"name":"CSE","grade":"2","ects":"6"}]},{"name":"Anwendungsfach","grades":[{"name":"NumA1","grade":"2","ects":"6"},{"name":"MathePraktikum","grade":"2","ects":"6"},{"name":"Cobra","grade":"2","ects":"10"}]}]}'
+    // ),
+    // "MSc 2010": JSON.parse(
+    //   '{"meggs":[{"name":"PraktischeInformatik","grades":[{"name":"Programmierung","grade":"4","ects":"8"},{"name":"DSAL","grade":"2","ects":"8"},{"name":"Datenbanken","grade":"2","ects":"6"},{"name":"SWT","grade":"2","ects":"6"}]},{"name":"TechnischeInformatik","grades":[{"name":"TI","grade":"2","ects":"6"},{"name":"BUS","grade":"2","ects":"6"},{"name":"Datkom","grade":"2","ects":"6"}]},{"name":"TheoretischeInformatik","grades":[{"name":"Fosap","grade":"2","ects":"6"},{"name":"BuK","grade":"2","ects":"7"},{"name":"Malo","grade":"4","ects":"7"}]},{"name":"Mathematik","grades":[{"name":"DS","grade":"2","ects":"6"},{"name":"AfI","grade":"2","ects":"8"},{"name":"LA","grade":"2","ects":"6"},{"name":"Stocha","grade":"2","ects":"6"}]},{"name":"Sonstiges","grades":[{"name":"Proseminar","grade":"2","ects":"3"},{"name":"Seminar","grade":"2","ects":"5"}]},{"name":"Wahlpflicht","grades":[{"name":"DPN","grade":"2","ects":"6"},{"name":"AI","grade":"2","ects":"6"},{"name":"Compilerbau","grade":"2","ects":"6"},{"name":"CSE","grade":"2","ects":"6"}]},{"name":"Anwendungsfach","grades":[{"name":"NumA1","grade":"2","ects":"6"},{"name":"MathePraktikum","grade":"2","ects":"6"},{"name":"Cobra","grade":"2","ects":"10"}]}]}'
+    // ),
+  };
+
+  const [po, setPO] = useState("BSc 2022");
+  const [transcript, updateTranscript] = useState(initialTranscript[po]);
 
   // for the result modal
   const [show, setShow] = useState(false);
   const [cancellationRecommendation, setCancellationRecommendation] =
     useState("");
   const handleClose = () => setShow(false);
+
+  const handlePoButtonClick = (event) => {
+    setPO(event.target.value);
+    updateTranscript(initialTranscript[event.target.value]);
+  };
 
   const addCategory = () => {
     const emptyCategory = {
@@ -37,7 +61,7 @@ const App = () => {
   };
 
   const resetForm = () => {
-    updateTranscript(initialTranscript);
+    updateTranscript(initialTranscript[po]);
   };
 
   const updateGradeArea = (i) => {
@@ -78,7 +102,7 @@ const App = () => {
 
   return (
     <>
-      <Container className="col-12 col-lg-9 col-xl-7">
+      <Container className="col-12 col-lg-12 col-xl-9">
         <div className="app-header d-flex flex-wrap flex-column flex-sm-row align-items-stretch align-items-sm-center has-column-gap-3">
           <h1 className="fw-bold">
             Notenstreicher
@@ -93,6 +117,15 @@ const App = () => {
             <Button variant="secondary" onClick={resetForm}>
               Reset Form
             </Button>
+          </div>
+          <div className="d-flex flex-column flex-md-row align-items-stretch ms-md-auto btn-group pt-3 pt-md-0">
+            {
+              Object.keys(initialTranscript).map(thisPo => (
+                <Button key={thisPo} value={thisPo} variant={thisPo === po.toString() ? 'primary active' : 'primary'} onClick={handlePoButtonClick}>
+                  {thisPo}
+                </Button>
+              ))
+            }
           </div>
         </div>
 
